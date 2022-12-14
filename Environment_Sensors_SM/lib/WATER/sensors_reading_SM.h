b37e08031a256f26eee2a52dc747a442fb829eab
antoniofsf14 <94556069+antoniofsf14@users.noreply.github.com>
2022-12-14 15:12:38 +0000

    State_machine initialized

---
+++ Environment_Sensors_SM/lib/WATER/sensors_reading_SM.h
@@ -0,0 +1,15 @@
+#ifndef sensors_reading_SM_h
+#define sensors_reading_SM_h
+
+#include "../lib/WATER/water_sensors.h"
+//#include ambi_sensors
+
+
+
+//void check_MQTT
+void runSwitchCase(int timeMs);
+void WaterSensorsTasks(); // probably not necessary
+
+
+
+#endif
